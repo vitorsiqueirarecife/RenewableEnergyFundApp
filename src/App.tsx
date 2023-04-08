c0bc59c138ee9785617ec59390bc2ffc2496cb70
Vitor Melo Siqueira <vitormelosiqueira@gmail.com>
2023-04-08 16:04:42 -0300

    added ReduxProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Routes from './routes/root.routes';
 import SafeAreaView from './shared/components/SafeAreaView';
+import {Provider as ReduxProvider} from 'react-redux';
+import {store} from './shared/store';
 
 function App() {
   return (
-    <SafeAreaView backgroundColor="#FFF" flex={1}>
-      <Routes />
-    </SafeAreaView>
+    <ReduxProvider store={store}>
+      <SafeAreaView backgroundColor="#FFF" flex={1}>
+        <Routes />
+      </SafeAreaView>
+    </ReduxProvider>
   );
 }
 
