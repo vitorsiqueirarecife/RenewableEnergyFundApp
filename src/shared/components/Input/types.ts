a1235387afafb1ffdb5e6700aee512b2b809ed83
Vitor Melo Siqueira <vitormelosiqueira@gmail.com>
2023-04-08 14:32:39 -0300

    remove old input from shared

--- src/shared/components/Input/types.ts
+++
@@ -1,9 +0,0 @@
-import {InputHTMLAttributes} from 'react';
-
-export interface Props
-  extends InputHTMLAttributes<HTMLInputElement | HTMLTextAreaElement> {
-  title: string;
-  error?: boolean;
-  multiline?: boolean;
-  rows?: number;
-}
