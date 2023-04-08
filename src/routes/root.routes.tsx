785876abf572a69231adce8ec6b6983c7606f8c8
Vitor Melo Siqueira <vitormelosiqueira@gmail.com>
2023-04-08 13:46:03 -0300

    removed header fron login and register

--- src/routes/root.routes.tsx
+++ src/routes/root.routes.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useMemo} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {RootStackParamList} from './types';
@@ -8,13 +8,16 @@ import AssetDetails from '../modules/Asset/screens/AssetDetails';
 import Tabs from './tab.routes';
 
 function Routes() {
-  const isLogged = true;
+  const isLogged = false;
   const Stack = createNativeStackNavigator<RootStackParamList>();
 
   return (
     <NavigationContainer>
       {!isLogged && (
-        <Stack.Navigator>
+        <Stack.Navigator
+          screenOptions={{
+            headerShown: false,
+          }}>
           <Stack.Screen name="Login" component={Login} />
           <Stack.Screen name="Register" component={Register} />
         </Stack.Navigator>
