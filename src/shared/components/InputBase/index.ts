0c74cd9ee99b327919a35e9060152fed7312c344
Vitor Melo Siqueira <vitormelosiqueira@gmail.com>
2023-04-08 14:32:49 -0300

    change base of inputbase

--- src/shared/components/InputBase/index.ts
+++ src/shared/components/InputBase/index.ts
@@ -1,31 +1,11 @@
 import styled from 'styled-components';
 import {border} from 'styled-system';
-
+import {TextInput} from 'react-native';
 import {InputBaseProps} from './types';
 
-export const InputBase = styled.input<InputBaseProps>`
+export const InputBase = styled(TextInput)<InputBaseProps>`
   ${border}
-  display: block;
-  border-radius: 4px;
-  margin: 0;
-  background: none;
-  width: 100%;
-  font-size: 13px;
-  padding: 22px;
-  color: white;
-  font-size: 18px;
-  font-weight: 500px;
-`;
-
-export const TextareBase = styled.textarea<InputBaseProps>`
-  ${border}
-  display: block;
-  border-radius: 4x;
-  margin: 0;
-  background: none;
-  width: 100%;
-  padding: 22px;
-  color: white;
-  font-size: 18px;
-  font-weight: 500px;
+  background-color: #e8e8e8;
+  padding-left: 10px;
+  padding-right: 10px;
 `;
