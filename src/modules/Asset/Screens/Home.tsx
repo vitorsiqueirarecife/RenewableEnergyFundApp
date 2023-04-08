725e3ab5b0bfadf7271536e8cfc10f4f5004543d
Vitor Melo Siqueira <vitormelosiqueira@gmail.com>
2023-04-08 12:02:02 -0300

    created basic page struct

---
+++ src/modules/Asset/Screens/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Box from '../../../shared/components/Box';
+
+const Asset = () => {
+  return (
+    <Box flex={1} backgroundColor="red">
+      <Box />
+    </Box>
+  );
+};
+
+export default Asset;
---
+++ src/modules/Wallet/Screens/Portfolio.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Box from '../../../shared/components/Box';
+
+const Portfolio = () => {
+  return (
+    <Box flex={1} backgroundColor="red">
+      <Box />
+    </Box>
+  );
+};
+
+export default Portfolio;
