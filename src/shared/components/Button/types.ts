48074efe519b785fea6b2b94795d46f31573b8cb
Vitor Melo Siqueira <vitormelosiqueira@gmail.com>
2023-04-08 15:28:47 -0300

    change variant can undefined

--- src/shared/components/Button/types.ts
+++ src/shared/components/Button/types.ts
@@ -9,7 +9,7 @@ type Variant = 'primary' | 'secondary' | 'success';
 
 export interface Props extends TouchableOpacityProps {
   children: string;
-  variant: Variant;
+  variant?: Variant;
 }
 
 export type Style = {
