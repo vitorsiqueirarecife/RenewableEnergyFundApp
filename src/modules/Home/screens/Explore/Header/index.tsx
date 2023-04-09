97c4509bcd6b31a7f2fea2e9b050d3a2c88db3db
Vitor Melo Siqueira <vitormelosiqueira@gmail.com>
2023-04-09 09:48:40 -0300

    explore header created

--- src/modules/Home/screens/Explore/Header/index.tsx
+++ src/modules/Home/screens/Explore/Header/index.tsx
@@ -1,8 +1,33 @@
 import React from 'react';
+import {TouchableOpacity} from 'react-native';
 import Box from '../../../../../shared/components/Box';
+import {Icon} from '../../../../../shared/components/Icon';
+import Typography from '../../../../../shared/components/Typography';
 
 const Header = () => {
-  return <Box flex={1} height={86} marginTop={2} backgroundColor="blue" />;
+  return (
+    <Box display="flex" flexDirection="row" height={65} marginTop={2}>
+      <Box display="flex" justifyContent="center" alignItems="center" flex={1}>
+        <TouchableOpacity>
+          <Icon
+            size="extraLarge1"
+            color="#3a3a3a"
+            name="account-circle-outline"
+          />
+        </TouchableOpacity>
+      </Box>
+      <Box display="flex" justifyContent="center" alignItems="center" flex={4}>
+        <Typography fontWeight="600" color="#000000">
+          Account: $1,457.23
+        </Typography>
+      </Box>
+      <Box display="flex" justifyContent="center" alignItems="center" flex={1}>
+        <TouchableOpacity>
+          <Icon size="extraLarge" color="#3a3a3a" name="bell-outline" />
+        </TouchableOpacity>
+      </Box>
+    </Box>
+  );
 };
 
 export default Header;
