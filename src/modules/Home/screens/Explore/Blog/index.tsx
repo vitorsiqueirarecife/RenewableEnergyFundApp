67850c7bed454b5a609b20ed45f1e8a5566a7507
Vitor Melo Siqueira <vitormelosiqueira@gmail.com>
2023-04-09 12:30:44 -0300

    bloc compoennt created

--- src/modules/Home/screens/Explore/Blog/index.tsx
+++ src/modules/Home/screens/Explore/Blog/index.tsx
@@ -1,8 +1,33 @@
 import React from 'react';
 import Box from '../../../../../shared/components/Box';
+import BlogItem from './BlogItem';
+import {mockBlogItems} from './mock';
 
 const Blog = () => {
-  return <Box flex={1} height={245} marginTop={2} backgroundColor="blue" />;
+  return (
+    <Box
+      display="flex"
+      flexDirection="row"
+      justifyContent="space-between"
+      height={215}
+      paddingY={10}
+      paddingX={20}
+      flexWrap="wrap">
+      {mockBlogItems.map(
+        (item, i) => (
+          <Box
+            display="flex"
+            alignItems={i % 2 === 0 ? 'flex-start' : 'flex-end'}
+            width="50%"
+            key={item.id}
+            marginBottom={10}>
+            <BlogItem title={item.title} />
+          </Box>
+        ),
+        [],
+      )}
+    </Box>
+  );
 };
 
 export default Blog;
---
+++ src/modules/Home/screens/Explore/Blog/mock.ts
@@ -0,0 +1,12 @@
+import {BlogItem} from './BlogItem/types';
+
+export const mockBlogItems: BlogItem[] = [
+  {
+    id: '4273498237',
+    title: 'Why should you invest here?',
+  },
+  {
+    id: '480238042',
+    title: 'Why should you invest here?',
+  },
+];
